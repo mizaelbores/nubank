d53faf672fbdd16d4e77c80ff6deedec50eec4f7
mizaelbores <72895452+mizaelbores@users.noreply.github.com>
2020-11-23 18:01:12 -0300

    nubank

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public options: Array<any> = [
+    { icon: 'person-add-outline', text: 'indicar amigos' },
+    { icon: 'phone-portrait-outline', text: 'Recarga de celular'},
+    { icon: 'wallet-outline', text: 'Depositar'},
+    { icon: 'options-outline', text: 'Ajustar limite'},
+    { icon: 'help-circle-outline', text: 'Me ajuda' },
+    { icon: 'barcode-outline', text: 'Pagar'},
+    { icon: 'lock-open-outline', text: 'Bloquear Cartão'},
+    { icon: 'car-outline', text: 'Cartão virtual'}
+  ];
+  public slidesOptions: any = {slidesPerView: 3, freeMode: true};
+  public itens: Array<any> = [
+    { icon: 'help-circle-outline', text: 'Me ajuda' },
+    { icon: 'person-outline', text: 'Perfil' },
+    { icon: 'cash-outline', text: 'Configurar conta' },
+    { icon: 'help-circle-outline', text: 'Me ajuda' }
+  ];
 
-  constructor() {}
+  constructor () { }
 
 }
